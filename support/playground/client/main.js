01b70696dd51ee89a29bc6bbd2cffeab606716e1
Vlada <vladimir.dubsky@hpe.com>
2016-07-20 18:54:20 +0200

    CUBIC INTERPOLATION

---
+++ support/playground/client/main.js
@@ -0,0 +1,24 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import './main.html';
+
+Template.hello.onRendered(function helloOnCreated() {
+  var ChartNatural=new SplineChart('chart_natural', 'X', 'F(x)', '#0f0', -100, 100, function(xs, ys) {
+    return new CubicSpline(xs, ys);
+  });
+  ChartNatural.point(new Question().answer(0),  0.5);
+  ChartNatural.point(new Question().answer(2),  0.25);
+  ChartNatural.point(new Question().answer(6),  0.5);
+  ChartNatural.point(new Question().answer(9.6),  0.75);
+  ChartNatural.point(new Question().answer(24), 0.5);
+
+  ChartNatural.draw(true);
+});
+
+
+
+Template.hello.helpers({
+});
+
+Template.hello.events({
+});
