a48d9982da2fcde5a20f58d137ea6d72070ed71f
dubsky <v.dubsky@gmail.com>
2016-10-15 00:05:05 +0200

    build improvements

--- install/electrify/electrify.bat
+++ install/electrify/electrify.bat
@@ -1,6 +1,8 @@
 cd ../../src
 SET TEMP=F:\Temp
 echo Temp location: %TEMP%
+echo CWD: %cd%
 call electrify package
+echo Electrify done
 cd ../install/electrify
 cp -R resources ../../src/.electrify/.dist/robolos-win32-ia32
