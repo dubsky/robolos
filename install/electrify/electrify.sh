029e99c5921781a7e1a971bee465a70c2f74f643
Vlada <vlada@sierra.doma.cz>
2016-10-14 19:08:02 +0200

    osx electrify

---
+++ install/electrify/electrify.sh
@@ -0,0 +1,8 @@
+cd ../../src
+electrify package
+cd ../install/electrify
+DIR=../../src/.electrify/.dist/robolos-darwin-x64
+cp -R resources.osx/Contents $DIR/robolos.app
+rm -rf out
+mkdir out
+hdiutil create -srcfolder $DIR out/robolos.dmg
---
+++ install/osx-build.sh
@@ -0,0 +1,6 @@
+cd ../src
+meteor npm install
+cd install
+cd electrify
+sh <electrify.sh
+cd ..
