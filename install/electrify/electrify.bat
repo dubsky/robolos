4d6ec423d29451dca2c280864943d206a59a9a55
dubsky <v.dubsky@gmail.com>
2016-10-18 17:34:06 +0200

    build improvements

--- install/electrify/electrify.bat
+++ install/electrify/electrify.bat
@@ -3,7 +3,7 @@ rm -rf .electrify/.dist
 rem SET TEMP=F:\Temp
 echo Temp location: %TEMP%
 echo CWD: %cd%
-call electrify package
+call electrify package -- --icon=public\favicon\favicon-96x96.ico
 echo Electrify done
 cd ../install/electrify
 echo CWD: %cd%
--- install/electrify/electrify.sh
+++ install/electrify/electrify.sh
@@ -1,5 +1,5 @@
 cd ../../src
-electrify package
+electrify package -- --icon=public\favicon\favicon-96x96.icns
 cd ../install/electrify
 DIR=../../src/.electrify/.dist/robolos-darwin-x64
 cp -R resources.osx/Contents $DIR/robolos.app
