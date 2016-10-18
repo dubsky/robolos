935e1f080f4742dc34fc66b0621c6ce3d4aba923
dubsky <v.dubsky@gmail.com>
2016-10-18 20:07:34 +0200

    build improvements

--- install/windows-build.bat
+++ install/windows-build.bat
@@ -1,6 +1,6 @@
 cd ..\src
-rem call meteor npm install
-call meteor build ../out --server localhost:3000
+call meteor npm install
+rem call meteor build ../out --server localhost:3000
 cd ..
 cd install
 cd electrify
