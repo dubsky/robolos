9bac0893ee2e17264367ff2c0b28d2c40900fce6
dubsky <v.dubsky@gmail.com>
2016-10-19 10:47:19 +0200

    build improvements

--- install/linux-build.sh
+++ install/linux-build.sh
@@ -4,3 +4,6 @@ meteor npm install
 meteor add-platform android
 meteor build ../output --server http://ec2-52-39-102-127.us-west-2.compute.amazonaws.com:3000
 mv ../output/src.tar.gz ../output/robolos-$OS-$ARCH-$VERSION.tar.gz
+cd ../output/build/android/project
+./gradlew
+mv build/outputs/android-debug.apk ../../../robolos-$VERSION.apk
