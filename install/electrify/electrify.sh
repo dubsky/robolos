dff5f3d7e00e8c32c4a28451ed3000a50ef45434
dubsky <v.dubsky@gmail.com>
2016-10-20 19:17:50 +0200

    build improvements, compression added

--- install/electrify/electrify.sh
+++ install/electrify/electrify.sh
@@ -5,5 +5,5 @@ DIR=../../src/.electrify/.dist/robolos-darwin-x64
 cp -R resources.osx/Contents $DIR/robolos.app
 rm -rf out
 mkdir out
-/usr/bin/hdiutil create -srcfolder $DIR out/robolos-$VERSION.dmg
+/usr/bin/hdiutil create zlib-level=9 -format UDZO -srcfolder $DIR out/robolos-$VERSION.dmg
 
