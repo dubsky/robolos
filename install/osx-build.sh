c19da471e7c057a4e194711fa2483401582388aa
U-Eman\Vlada <v.dubsky@gmail.com>
2016-10-20 18:16:22 +0200

    wb

--- install/osx-build.sh
+++ install/osx-build.sh
@@ -2,6 +2,8 @@ pwd
 cd ../src
 export PATH=$PATH:/usr/local/bin
 meteor npm install
+meteor add-platform ios
+meteor build ../output --server http://ec2-52-39-102-127.us-west-2.compute.amazonaws.com:3000
 cd ../install
 cd electrify
 sh <electrify.sh
