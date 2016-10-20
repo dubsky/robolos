4131c029643a897eb206875c737173b659e17bb7
dubsky <v.dubsky@gmail.com>
2016-10-20 21:48:56 +0200

    build improvements, compression added

--- install/osx-build.sh
+++ install/osx-build.sh
@@ -3,9 +3,9 @@ pwd
 cd ../src
 export PATH=$PATH:/usr/local/bin
 meteor npm install
-meteor add-platform ios
-meteor build ../output --server http://ec2-52-39-102-127.us-west-2.compute.amazonaws.com:3000
 cd ../install
 cd electrify
 sh <electrify.sh
-cd ..
+cd ../../src
+meteor add-platform ios
+meteor build ../output --server http://ec2-52-39-102-127.us-west-2.compute.amazonaws.com:3000
