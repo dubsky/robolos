e321f01b9d82e598ebf59aac01aa0ec15163f35e
dubsky <v.dubsky@gmail.com>
2016-10-20 21:23:17 +0200

    build improvements, compression added

--- install/linux-build.sh
+++ install/linux-build.sh
@@ -5,5 +5,6 @@ meteor add-platform android
 meteor build ../output --server http://ec2-52-39-102-127.us-west-2.compute.amazonaws.com:3000
 mv ../output/src.tar.gz ../output/robolos-$OS-$ARCH-$VERSION.tar.gz
 cd ../output/build/android/project
+gradle wrapper --gradle-version 2.0
 ./gradlew
 mv build/outputs/android-debug.apk ../../../robolos-$VERSION.apk
