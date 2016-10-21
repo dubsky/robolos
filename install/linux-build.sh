3dbf6a850e0615d129bf5db1dd23568f75ba97c1
dubsky <v.dubsky@gmail.com>
2016-10-21 14:58:16 +0200

    build improvements, os x

--- install/linux-build.sh
+++ install/linux-build.sh
@@ -7,8 +7,9 @@ echo Building
 meteor build ../output --server http://ec2-52-39-102-127.us-west-2.compute.amazonaws.com:3000
 echo Build finished
 mv ../output/src.tar.gz ../output/robolos-$OS-$ARCH-$VERSION.tar.gz
-cd ../output/android/project
-echo Starting Gradle
-gradle build
-echo Gradle finished
-mv build/outputs/android-debug.apk ../../robolos-$VERSION.apk
+#cd ../output/android/project
+#echo Starting Gradle
+#gradle -version
+#gradle build
+#echo Gradle finished
+mv ../output/android/release-unsigned.apk ../output/robolos-$VERSION.apk
