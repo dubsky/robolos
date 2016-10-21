479894c25a1712d16411a94d11190e4001161458
dubsky <v.dubsky@gmail.com>
2016-10-21 10:48:27 +0200

    build improvements

--- install/osx-build.sh
+++ install/osx-build.sh
@@ -3,13 +3,13 @@ pwd
 cd ../src
 export PATH=$PATH:/usr/local/bin
 meteor npm install
-echo Add iOS
-meteor add-platform ios
-echo Building
-meteor build ../output --server http://ec2-52-39-102-127.us-west-2.compute.amazonaws.com:3000
 echo Electrify
 cd ../install
 cd electrify
 sh <electrify.sh
 cd ../../src
+echo Adding ios
+meteor add-platform ios
+echo Building
+meteor build ../output --server http://ec2-52-39-102-127.us-west-2.compute.amazonaws.com:3000
 echo Build finished
