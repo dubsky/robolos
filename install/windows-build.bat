a77538a22a2da6b5cb5f6c28d4cedfa15fc2262d
dubsky <v.dubsky@gmail.com>
2016-11-28 21:39:59 +0100

    build improvements

--- install/windows-build.bat
+++ install/windows-build.bat
@@ -2,7 +2,7 @@ cd ..\src
 rem serialport previously installed using -g
 rem call meteor npm install -g serialport@1.7.4
 echo METEOR NPM INSTALL
-call meteor npm install
+call npm install
 rem echo METEOR BUILD
 rem call meteor build ../out --server localhost:3000
 cd ..
