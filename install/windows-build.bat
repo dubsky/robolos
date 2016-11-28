8529669b09f7b03f23ce59a1a4cd89b20537de3c
dubsky <v.dubsky@gmail.com>
2016-11-28 20:58:35 +0100

    New changelist

--- install/windows-build.bat
+++ install/windows-build.bat
@@ -1,8 +1,10 @@
 cd ..\src
 rem serialport previously installed using -g
 call npm install serialport@1.7.4
+echo METEOR NPM INSTALL
 call meteor npm install
-rem call meteor build ../out --server localhost:3000
+echo METEOR BUILD
+call meteor build ../out --server localhost:3000
 cd ..
 cd install
 cd electrify
