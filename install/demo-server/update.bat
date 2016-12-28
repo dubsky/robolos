300d6d282534e6a771acb4c106cc03f97b42d53c
dubsky <v.dubsky@gmail.com>
2016-12-28 09:14:29 +0100

    smaller improvements

--- install/demo-server/update.bat
+++ install/demo-server/update.bat
@@ -1,9 +1,10 @@
 mkdir tmp
 cd tmp
-wget http://192.168.1.146:8080/job/robolos-linux-intel/lastSuccessfulBuild/artifact/output/robolos-linux-intel-1.0.tar.gz
+wget http://elementary:8080/job/robolos-linux-intel/lastSuccessfulBuild/artifact/output/robolos-linux-intel-1.0.tar.gz
 SET HOST=ec2-35-163-254-23.us-west-2.compute.amazonaws.com
-scp -i ../../keys/robolos-demo.pem robolos-linux-intel-1.0.tar.gz ubuntu@%HOST%:/home/robolos
-ssh ubuntu@%HOST% 'sudo /home/ubuntu/update.sh'
+chmod 400 ../../keys/robolos-cloud.pem
+scp -i ../../keys/robolos-cloud.pem robolos-linux-intel-1.0.tar.gz ubuntu@%HOST%:/home/ubuntu
+#ssh ubuntu@%HOST% 'sudo /home/ubuntu/update.sh'
 
 
 
